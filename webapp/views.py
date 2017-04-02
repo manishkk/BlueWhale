c5f707fd4b8a18c620b630b9c54c9283722622e2
Manish Kumar <manishkk@users.noreply.github.com>
2017-04-02 21:45:03 +0200

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,11 @@
-
 import socket
 from django.http import HttpResponse
 import sys
+import time
+import struct
 
-
-#from general import *
-sys.path.append("./webapp/")
+# from general import *
+sys.path.append ( "./webapp/" )
 from networking.ethernet import Ethernet
 from networking.ipv4 import IPv4
 from networking.icmp import ICMP
@@ -14,50 +14,32 @@ from networking.udp import UDP
 from networking.pcap import Pcap
 from networking.http import HTTP
 
-
-pcap = Pcap('Test.pcap')
-s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-
-
-
-
-#print('Ethernet Frame:')
-#print('Destination: {}, Source: {}, Protocol: {}'.format(eth.dest_mac, eth.src_mac, eth.proto))
-
- #if eth.proto == 8:
-           # ipv4 = IPv4(eth.data)
-           # print('IPv4 Packet:')
-           #   print('Version: {}, Header Length: {}, TTL: {},'.format(ipv4.version, ipv4.header_length, ipv4.ttl))
-           #   print('Protocol: {}, Source: {}, Target: {}'.format(ipv4.proto, ipv4.src, ipv4.target))
-
+pcap = Pcap ( 'Test.pcap' )
+s = socket.socket ( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs ( 3 ) )
 
 
 def index(request):
-    answer = []
-    for raw_data in range(5):
-        raw_data , addr = s.recvfrom(65565)
-        pcap.write(raw_data)
-        eth = Ethernet(raw_data)
-        ipv4 = IPv4(eth.data)
+    answer = ''
+
+    answer.append ( str (
+        '<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style></head><body><table><tr><th>Time Stamp</th><th>Destination MAC</th><th>Source MAC</th><th>Protocol</th><th>IP Version</th><th>IP Header Length</th><th>TTL</th><th>Protocol</th><th>Source IP</th><th>Target IP</th><th>Source Port</th><th>Destination Port</th><th>Length</th></tr>' ) )
+    for raw_data in range ( 5 ):
+        raw_data , addr = s.recvfrom ( 65565 )
+        pcap.write ( raw_data )
+        unpacked = struct.unpack ( '@ I H H i I I I I I I I' , raw_data[:40] )
+        timestamp = time.strftime ( '%Y-%m-%d %H:%M:%S' , time.localtime ( unpacked ) )
+        eth = Ethernet ( raw_data )
+        ipv4 = IPv4 ( eth.data )
         icmp = ICMP ( ipv4.data )
-        tcp = TCP (ipv4.data)
-        udp = UDP (ipv4.data)
-       # pcap =Pcap(pcap.data)
+        tcp = TCP ( ipv4.data )
+        udp = UDP ( ipv4.data )
 
-        answer.extend ( [
-              
+        answer.append ( str (
+            '<tr><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th><th>{}</th></tr>'.format (
+                timestamp , eth.dest_mac , eth.src_mac , eth.proto , ipv4.version , ipv4.header_length , ipv4.ttl ,
+                ipv4.proto , ipv4.src , ipv4.target , tcp.src_port , tcp.dest_port , udp.src_port , udp.dest_port ,
+                udp.size ) ) )
 
-                str ('<head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style></head><body><table>' ) ,
-                str ('<tr><th></th><th>Time Stamp:</th><th></th></tr>'.format ( ) ) ,
-                str ('<tr><th>Destination MAC: {}</th><th>Source MAC: {}</th><th>Protocol: {}</th></tr>'.format (eth.dest_mac , eth.src_mac , eth.proto ) ) ,
-                str ('<tr><th>IP Version: {}</th><th>IP Header Length: {}</th><th>TTL: {}</th></tr>'.format ( ipv4.version , ipv4.header_length ,ipv4.ttl ) ) ,
-                str ('<tr><th>Protocol: {}</th><th>Source IP: {}</th><th>Target IP: {}</th></tr>'.format ( ipv4.proto ,ipv4.src ,ipv4.target ) ) ,
-                str ('<tr><th>Source Port: {}</th><th>Destination Port: {}</th><th></th></tr>'.format ( tcp.src_port ,tcp.dest_port ) ) ,
-                str ('<tr><th>Source Port: {}</th><th>Destination Port: {}</th><th>Length: {}</th></tr>'.format (udp.src_port , udp.dest_port , udp.size ) ) ,
-                str ( '</table></body>' )
+    answer.append ( str ( '</table></body>' ) )
 
-        ])
-
-        answer.append (" ")
-
-    return HttpResponse("<br>".join(answer))
+    return HttpResponse ( answer )
